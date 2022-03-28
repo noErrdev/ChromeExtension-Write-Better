946310990947f7bbd5cf25c71f71a80133fcd12b
noErrdev <dbongo0701@gmail.com>
2022-03-27 22:15:01 -0700

    Update appscript form.
    The issue is that formData is empty

--- gh-pages/uninstall.js
+++ gh-pages/uninstall.js
@@ -1,16 +1,24 @@
 const scriptURL =
-    'https://script.google.com/macros/s/AKfycbyd4PLN9WMHXL9dqR7zAOq-jV3oi5uHvWUm2s58lVK3AyMKveYRMQzoLpW7m2Hzb2Xw/exec'
+    'https://script.google.com/macros/s/AKfycbwhHQ92h2SCoXLRKnWOmCnDdU8-00jG_npynrgBEm5JIe01G0R-POcXI-ZJDH2Bd7Ak/exec?missing_feature=no&'
 const extensionUrl = 'https://justiceo.github.io/chrome-extension-starter';
 const form = document.forms['google-sheet'];
+const formData = new FormData(form);
 
 form.addEventListener('submit', e => {
     e.preventDefault();
-    console.log(new FormData(form));
-    fetch(scriptURL, {
-        method: 'POST',
-        body: new FormData(form)
+    console.log("Submitting data: ", form, formData);
+
+    let dataUrl = scriptURL;
+    formData.forEach(file => console.log("File: ", file));
+    for( let pair of formData.entries() ) {
+        console.log(pair);
+        dataUrl = dataUrl + encodeURIComponent( pair[0] ) + '=' + encodeURIComponent( pair[1] ) + "&";
+    }
+    fetch(dataUrl, {
+        method: 'GET', 
     }).then(response => {
+        console.log("Response: ", response);
         form.reset();
-        window.location.href = extensionUrl;
+        // window.location.href = extensionUrl;
     }).catch(error => console.error('Error!', error.message));
 })
